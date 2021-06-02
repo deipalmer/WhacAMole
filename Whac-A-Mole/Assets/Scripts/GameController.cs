8cf0f9008ff4822fd7c2d6411f1bb705155df0c1
Eliseo <eliseoprofe1988@gmail.com>
2021-06-02 17:23:07 +0100

    First commit

---
+++ Whac-A-Mole/Assets/Scripts/GameController.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+    public GameObject mainMenu, inGameUI,endScreen,recordPanel;
+
+    public Transform molesParent;
+    private MoleBehaviour[] moles;
+
+    public bool playing = false;
+
+    public float gameDuration = 60f;
+    public float timePlayed;
+
+    int points = 0;
+    int clicks = 0;
+    int failedClicks = 0;
+
+    public TMP_InputField nameField;
+    string playerName;
+
+    public TextMeshProUGUI infoGame;
+
+    void Awake()
+    {
+        if (GameController.instance == null)
+        {
+            ConfigureInstance();
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+    }
+
+    void ConfigureInstance()
+    {
+        //Configura acceso a moles
+        moles = new MoleBehaviour[molesParent.childCount];
+        for (int i = 0; i < molesParent.childCount; i++)
+        {
+            moles[i] = molesParent.GetChild(i).GetComponent<MoleBehaviour>();
+        }
+
+        //Inicia los puntos
+        points = 0;
+        clicks = 0;
+        failedClicks = 0;
+
+        //Activa la UI inicial
+        inGameUI.SetActive(false);
+        mainMenu.SetActive(true);
+        endScreen.SetActive(false);
+        recordPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playing == true)
+        {
+            timePlayed += Time.deltaTime;
+
+            if (timePlayed >= gameDuration)
+            {
+
+                ShowEndScreen();
+                playing = false;
+                for (int i = 0; i < moles.Length; i++)
+                {
+                    moles[i].StopMole();
+                }
+
+                
+            }
+            else
+            {
+                CheckClicks();
+            }
+            
+        }
+    }
+
+
+    void ShowEndScreen()
+    {
+        endScreen.SetActive(true);
+        infoGame.text = " Total points : " + "000" + "\n Record: " + "100" + "\n 10" + "% good shots \n" + "999" + " bad shots";
+
+        bool isRecord = false;
+        //si hay nuevo record mostrar el panel recordPanel
+        recordPanel.SetActive(isRecord);
+    }
+
+    /// <summary>
+    /// Function called from End Screen when players hits Retry button
+    /// </summary>
+    public void Retry()
+    {
+        //Guardar record si es necesario
+
+        //Acceso al texto escrito
+        playerName = nameField.text;
+        Debug.Log("Record de " + playerName);
+
+        //Reinicia información del juego
+        ResetGame();
+        //Cambia las pantallas
+        inGameUI.SetActive(true);
+        mainMenu.SetActive(false);
+        endScreen.SetActive(false);
+        //Activa juego
+        playing = true;
+
+        //Reinicia moles
+        for (int i = 0; i < moles.Length; i++)
+        {
+            moles[i].ResetMole();
+        }
+    }
+
+    /// <summary>
+    /// Restarts all info game
+    /// </summary>
+    void ResetGame()
+    {
+        for (int i = 0; i < moles.Length; i++)
+        {
+            moles[i].StopMole();
+        }
+
+        timePlayed = 0.0f;
+        points = 0;
+        clicks = 0;
+        failedClicks = 0;
+    }
+
+    public void EnterMainScreen()
+    {
+        //Reinicia información del juego
+        ResetGame();
+        //Cambia las pantallas
+        inGameUI.SetActive(false);
+        mainMenu.SetActive(true);
+        endScreen.SetActive(false);
+        recordPanel.SetActive(false);
+
+    }
+
+    /// <summary>
+    /// Used to check if players hits or not the moles/powerups
+    /// </summary>
+    public void CheckClicks()
+    {
+        if ((Input.touchCount >= 1 && Input.GetTouch(0).phase == TouchPhase.Ended) || (Input.GetMouseButtonUp(0)))
+        {
+          
+            Vector3 pos = Input.mousePosition;
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                pos = Input.GetTouch(0).position;
+            }
+
+            Ray rayo = Camera.main.ScreenPointToRay(pos);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(rayo, out hitInfo))
+            {
+                if (hitInfo.collider.tag.Equals("Mole"))
+                {
+                    MoleBehaviour mole = hitInfo.collider.GetComponent<MoleBehaviour>();
+                    mole.OnHitMole();
+                }
+            }
+        }
+    }
+
+    public void OnGameStart()
+    {
+        mainMenu.SetActive(false);
+        inGameUI.SetActive(true);
+        points = 0;
+        for (int i = 0; i < moles.Length; i++)
+        {
+            moles[i].ResetMole(moles[i].initTimeMin, moles[i].initTimeMax);
+        }
+        playing = true;
+    }
+
+    /// <summary>
+    /// Funcion para entrar en pausa, pone playing en false y muestra la pantalla de pausa.
+    /// </summary>
+    public void EnterOnPause()
+    { 
+    
+    
+    }
+
+}
---
+++ Whac-A-Mole/Assets/Scripts/MoleBehaviour.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoleBehaviour : MonoBehaviour
+{
+    Vector3 originalPosition;
+    float timeToStartMoving = 1.0f;
+    bool isShown = false;
+    public float initTimeMin = 0.5f;
+    public float initTimeMax = 3.5f;
+    private bool moleIsRunning = true;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        originalPosition = transform.position;
+        ResetMole(initTimeMin, initTimeMax);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moleIsRunning)
+        {
+            timeToStartMoving -= Time.deltaTime;
+            if (timeToStartMoving <= 0.0f && isShown == false)
+            {
+                transform.position = originalPosition;
+                isShown = true;
+
+                timeToStartMoving = Random.Range(3.5f, 7.8f);
+            }
+            else if (timeToStartMoving <= 0.0f && isShown == true)
+            {
+                ResetMole(3.5f, 7.8f);
+            }
+        }
+       
+    }
+
+    public void ResetMole()
+    {
+
+        ResetMole(initTimeMin, initTimeMax);
+    }
+    public void ResetMole(float minTime, float maxTime)
+    {
+        moleIsRunning = true;
+        isShown = false;
+        Vector3 newPos = originalPosition;
+        newPos.y -= 1.0f;
+        transform.position = newPos;
+        timeToStartMoving = Random.Range(minTime, maxTime);
+    }
+
+    public void OnHitMole()
+    {
+        if (isShown == true)
+        {
+            ResetMole(3.5f, 7.8f);
+        }
+    }
+
+    public void StopMole()
+    {
+        moleIsRunning = false;
+        isShown = false;
+        Vector3 newPos = originalPosition;
+        newPos.y -= 1.0f;
+        transform.position = newPos;
+    }
+
+
+}
